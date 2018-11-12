45c92d9557721544c80a77e8c2e1bdbcd8182597
Jesse WB <JesseWB.C@gmail.com>
2018-11-11 22:48:28 -0800

    added subitem components to sidebar

--- client/src/components/Item.jsx
+++ client/src/components/Item.jsx
@@ -1,14 +1,36 @@
-import React, {Component} from 'react';
-
-import SubItem from './SubItem';
+import React, { Component } from "react";
+import SubItem from "./SubItem";
+import List from "./List"
 
 //Accordion logic happens here
+//Choose to display or not display subitems
+
 class Item extends Component {
-    render() {
-        return(
-            <h1>Item</h1>
-        );
-    }
+  
+  //to be managed by redux later
+  state = {
+    expanded: false
+  };
+
+  changeViewOnClick(e){
+
+  }
+
+  render() {
+    let {item} = this.props;
+    console.log(item);
+    let {expanded} = this.state;
+    let subItemList = item.items.map(subItemElement => {
+      return(<div key={subItemElement.id}>{subItemElement.name}</div>);
+    });
+
+    return (
+      <div>
+        <div onClick={this.changeViewOnClick}>{item.name}</div>
+        {subItemList}
+      </div>
+    );
+  }
 }
 
 export default Item;
